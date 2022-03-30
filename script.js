4ec87978086d828f3726e36e0d287bec1d4fdd5c
Facundo Barrera <barreraaybarfacundo@gmail.com>
2022-03-30 13:08:25 -0300

    Implement sketch funtionality
    Set event listeners on all squares (in function setGrid) and code funtion paintSquare

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // HTML elements
 const grid = document.querySelector('.grid');
+let gridSquares, gridLines;
 const colorsPalette = document.querySelector('.colors');
 const currentColorSelector = document.querySelector('.current-color');
 
@@ -7,42 +8,63 @@ const rgbRed = document.querySelector('#rgb-red');
 const rgbGreen = document.querySelector('#rgb-green');
 const rgbBlue = document.querySelector('#rgb-blue');
 
+// Variables
+
+let gridSize = 16;
+let color = 'red';
+
 // Functions
 
 function resetGrid()
 {
-    let gridSquares = document.querySelectorAll('.grid div.line');
-    gridSquares.forEach(square => grid.removeChild(square));
+    if (gridLines === undefined || gridLines === null) return;
+    gridLines.forEach(line => grid.removeChild(line));
 }
 
 function setGrid(size)
 {
+    gridSize = size;
+
     resetGrid();
 
     // Add squares
     let line, square;
 
-    for (let row = 0; row < size; row++)
+    for (let row = 0; row < gridSize; row++)
     {
         line = document.createElement('div');
         line.classList.add('line');
 
-        for (let col = 0; col < size; col++)
+        for (let col = 0; col < gridSize; col++)
         {
             square = document.createElement('div');
             square.classList.add('square');
+            square.setAttribute('data-id', `${col + (gridSize*row)}`);
 
             line.appendChild(square);
         }
 
         grid.appendChild(line);
     }
+
+    gridLines = document.querySelectorAll('.grid div.line');
+    gridSquares = document.querySelectorAll('.grid div.square');
+
+    setSquaresListeners();
+}
+
+function paintSquare(e)
+{
+    this.style['background-color'] = color;
 }
 
 // Event listeners
 
+function setSquaresListeners()
+{
+    gridSquares.forEach(square => square.addEventListener('mouseover', paintSquare));
+}
 
 // Grid setup
-let gridSize = 16;
 
 setGrid(gridSize);
